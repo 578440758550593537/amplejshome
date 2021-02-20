ca06394b93ecb97b3d3c39d784b22723ede12979
Dark <51209105+578440758550593537@users.noreply.github.com>
2021-02-20 10:24:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Hello my name is Arick!");
+document.write("Hello!");
+document.write('Who are you?');
+var name; 
+name = 'Arick';
+alert(name);
+var person = prompt("Введите место жительства:", "Днепр");
+document.write(`<div>Запомнил где вы живете ${person}</div>`)
+console.log(`Запомнил где вы живете: ${person}`)
